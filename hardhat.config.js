a1c27efeced00fca222e2fc884a2835bb1e2fdb8
Guru Singh <127821747+gurusinghpal@users.noreply.github.com>
2023-07-15 00:03:07 +0530

    Add files via upload

---
+++ hardhat.config.js
@@ -0,0 +1,44 @@
+require("@nomicfoundation/hardhat-toolbox");
+require('dotenv').config();
+
+const FORK_FUJI = false;
+const FORK_MAINNET = false;
+let forkingData = undefined;
+
+if (FORK_MAINNET) {
+  forkingData = {
+    url: "https://api.avax.network/ext/bc/C/rpcc",
+  };
+}
+if (FORK_FUJI) {
+  forkingData = {
+    url: "https://api.avax-test.network/ext/bc/C/rpc",
+  };
+}
+
+/** @type import('hardhat/config').HardhatUserConfig */
+module.exports = {
+  solidity: "0.8.18",
+  networks: {
+    hardhat: {
+      gasPrice: 225000000000,
+      chainId: !forkingData ? 43112 : undefined, //Only specify a chainId if we are not forking
+      forking: forkingData,
+    },
+    fuji: {
+      url: "https://api.avax-test.network/ext/bc/C/rpc",
+      gasPrice: 225000000000,
+      chainId: 43113,
+      accounts: ["689af8efa8c651a91ad287602527f3af2fe9f6501a7ac4b061667b5a93e037fd"], // we use a .env file to hide our wallets private key
+    },
+    mainnet: {
+      url: "https://api.avax.network/ext/bc/C/rpc",
+      gasPrice: 225000000000,
+      chainId: 43114,
+      accounts: ["689af8efa8c651a91ad287602527f3af2fe9f6501a7ac4b061667b5a93e037fd"],
+    },
+  },
+  etherscan: {
+    apiKey: "ASDM8KFKJ1G6MJDTN4EN9UVB59SKSFT9B4", // we use an .env file to hide our Snowtrace API KEY
+  },
+};
